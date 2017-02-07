9eadb5dca57bdffa5653bc3b6c1492198f0d1eef
Brendan A R Sechter <b-sechter@unext.jp>
2017-02-07 11:19:05 +0900

    Optimized imports.

--- app/src/main/kotlin/com/sennue/hellokotlin/MainActivity.kt
+++ app/src/main/kotlin/com/sennue/hellokotlin/MainActivity.kt
@@ -2,9 +2,8 @@ package com.sennue.hellokotlin
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
